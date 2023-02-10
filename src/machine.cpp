506f7440cea60f788cbdfa3b006069931258c01a
Aran Roig <aranseraroig@gmail.com>
2023-02-10 13:45:37 +0100

    Bueno eh millores hi ha suport per funcions!

--- src/machine.cpp
+++ src/machine.cpp
@@ -2,6 +2,17 @@
 
 #include <iostream>
 
+
+void That::VM::MemDump(uint8_t *data, int size){
+    std::cout << "[Dump] Size: " << size << std::endl;
+
+    for(int i = 0; i < size; i++){
+        std::cout << (int) *(data + i) << std::endl;
+    }
+    std::cout << "Si" << std::endl;
+    return;
+}
+
 That::VM::VM(char filename[]){
 
     FILE* f = fopen(filename, "r");
@@ -9,7 +20,9 @@ That::VM::VM(char filename[]){
     uint32_t nConst; // 32 bit
     fread(&nConst, sizeof(unsigned int), 1, f);
     // Read constants
-    char ins[4];
+
+    std::cout << "Ok tenim " <<  nConst << " constants" << std::endl;
+    uint8_t ins[4];
 
     uint8_t* cons;
     reg_t* regCons[nConst];
@@ -20,26 +33,38 @@ That::VM::VM(char filename[]){
         uint32_t size, typeID; // Es pot cambiar això de tamany?
         fread(&typeID, sizeof(uint32_t), 1, f);
 
-        if(typeID == 1){
+        reg_t::type_t tipus = static_cast<reg_t::type_t>(typeID);
+        
+        std::cout << "Es de tipus " <<  typeID << std::endl;
+
+        if(tipus == reg_t::INT){
             fread(&(nReg->num), sizeof(nReg->num), 1, f);
         } else {
             fread(&size, sizeof(uint32_t), 1, f);
+            std::cout << "Te size " << size << std::endl;
+            nReg->num = size;
             
-            fread(&(nReg->lnum), sizeof(uint8_t), size, f);
+            nReg->data = new uint8_t[size];
+            fread(nReg->data, sizeof(uint8_t), size, f);
+            MemDump(nReg->data, size);
         }
         
-        nReg->type = static_cast<reg_t::type_t>(typeID);
+        nReg->type = tipus;
         regCons[i] = nReg;
         
     }
 
-    while(fread(ins, sizeof(char), 4, f)){
-        Process(ins, regCons);
+    while(fread(ins, sizeof(uint8_t), 4, f)){
+        std::cout << "Leido hasiendo dump" << std::endl;
+        MemDump(ins, 4);
+
+        Process(ins, regCons, 0);
     }
 }
 
-int That::VM::Process(char ins[], reg_t* regCons[]){
+int That::VM::Process(uint8_t ins[], reg_t* regCons[], int offset){
     int8_t instId = ((int8_t) ins[0]);
+    std::cout << "instId: " << (unsigned int) instId << std::endl;
     VM::Instruction tipus = static_cast<VM::Instruction>(instId);
 
     int8_t b, c;
@@ -48,46 +73,113 @@ int That::VM::Process(char ins[], reg_t* regCons[]){
 
     switch (tipus)
     {
-    case Instruction::PUSH:
+    case Instruction::PUSH: // Push reg
         abx = this->ReadAbx(ins);
+        std::cout << "Instruccion push " << abx << std::endl;
         if(abx == 0){
             // Push the none
             reg_t* none = new reg_t;
             none->type = reg_t::type_t::NONE;
             regs.push_back(none);
         } else{
-            regs.push_back(regCons[abx-1]);
+            std::cout << "Cola " << (uint32_t) abx << std::endl;
+            regs.push_back(regCons[abx]);
         }
         break;
-    case Instruction::MOVE:
-        a = this->ReadA(ins);
-        b = this->ReadB(ins);
-        c = this->ReadC(ins);
-        regs[c] = regs[b];
+    case Instruction::MOVE: // Move regs
+        a = this->ReadA(ins) + offset;
+        b = this->ReadB(ins) + offset;
+        regs[b] = regs[a];
+        break;
+    case Instruction::CALL: // User call
+        std::cout << "Vale caleado esta" << std::endl;
+
+        a = this->ReadA(ins) + offset;
+        b = this->ReadB(ins) + offset;
+        c = this->ReadC(ins) + offset;
+
+        Call(a, b, c);
+        break;
+    case Instruction::ICL: // Internal call
+        a = this->ReadA(ins) + offset;
+        b = this->ReadB(ins) + offset;
+        c = this->ReadC(ins) + offset;
+
+        // Aqui funcions internes o algo
         break;
     default:
         break;
     }
 }
 
-uint8_t That::VM::ReadA(char ins[]){
+void That::VM::Call(uint8_t a, uint8_t b, uint8_t c){
+    int n = regs[a]->num;
+
+    uint8_t *data = regs[a]->data;
+
+    for(int i = 0; i < c - b; i++){
+        regs.push_back(regs[i]);
+    }
+
+    void* pdata = &data;
+    uint32_t* cNums = reinterpret_cast<uint32_t*>(pdata);
+    uint32_t cNum = cNums[0];
+
+    reg_t* regCons[cNum];
+
+    int j = 0;
+    for(int i = 0; i < cNum; i++){
+        // Llegim un uint32_t de size
+        reg_t* nReg = new reg_t;
+        uint32_t size, typeID; // Es pot cambiar això de tamany?
+        
+        uint32_t* vData = reinterpret_cast<uint32_t*>((void*) (data + j + UINT32_SIZE));
+        typeID = *(vData);
+
+        reg_t::type_t tipus = static_cast<reg_t::type_t>(typeID);
+
+        if(tipus == reg_t::INT){
+            nReg->num = *(vData + 1);
+        } else {
+            nReg->num = *(vData + 1);
+            nReg->data = data + 2 * UINT32_SIZE;
+        }
+        
+        nReg->type = tipus;
+        regCons[i] = nReg;
+    }
+
+    int s = regs.size();
+
+    for(int i = 0; i < n - j; i++){
+        Process((regs[a]->data) + j + i, regCons, s);
+    }
+
+    // Eliminar vars
+    for(int i = 0; i < c - b; i++){
+        regs.pop_back();
+    }
+
+}
+
+uint8_t That::VM::ReadA(uint8_t ins[]){
     return ((uint8_t) ins[1]);
 }
 
-uint8_t That::VM::ReadB(char ins[]){
+uint8_t That::VM::ReadB(uint8_t ins[]){
     return ((uint8_t) ins[2]);
 }
 
-uint8_t That::VM::ReadC(char ins[]){
+uint8_t That::VM::ReadC(uint8_t ins[]){
     return ((uint8_t) ins[3]);
 }
 
-uint32_t That::VM::ReadBx(char ins[]){
+uint32_t That::VM::ReadBx(uint8_t ins[]){
     return ((uint32_t) ((uint8_t) (ins[2]) << 8) | (uint32_t) ((uint8_t) ins[3]));
 }
 
-uint32_t That::VM::ReadAbx(char ins[]){
-    return ((uint32_t) ((uint8_t) (ins[1]) << 16) | (uint32_t) ((uint8_t) (ins[2]) << 8) | (uint32_t) ((uint8_t) ins[3]));
+uint32_t That::VM::ReadAbx(uint8_t ins[]){
+    return ((uint32_t) ((uint8_t) (ins[1])) | (uint32_t) ((uint8_t) (ins[2]) << 8) | (uint32_t) ((uint8_t) (ins[3]) << 16));
 }
 
 That::VM::~VM(){
--- src/machine.h
+++ src/machine.h
@@ -3,18 +3,21 @@
 #include <cstdint>
 #include <vector>
 
+#define UINT32_SIZE 4
+
 namespace That {
     class VM {
         typedef struct reg {
-            union {
-                uint32_t num;
-                uint8_t *lnum;
-            };
+            
+            uint32_t num;
+            uint8_t *data;
+
             enum type_t {
                 OBJECT,
                 INT,
                 STR,
                 NUMBER,
+                FUNCTION,
                 NONE,
             } type;
         } reg_t;
@@ -26,17 +29,27 @@ namespace That {
             ~VM();
 
             enum Instruction {
-                PUSH, // A, Bx
+                PUSH, // abx
                 MOVE, // A, B
-                POP // Bx
+
+                CALL, // A, B, C // A(B+1, B+2, ..., B+C-1)
+                ICL, // A, B, C // A(B+1,B+2,...,B+C-1) // Internal call
+
+                ADD, // A = B + C
+                SUB, // A = B - C
+                MUL, // A = B * C
+                DIV, // A = B / C
             };
             
         private:
-            int Process(char ins[], reg_t* cons[]);
-            uint8_t ReadA(char ins[]);
-            uint8_t ReadB(char ins[]);
-            uint8_t ReadC(char ins[]);
-            uint32_t ReadBx(char ins[]);
-            uint32_t ReadAbx(char ins[]);
+            void MemDump(uint8_t *data, int size);
+
+            int Process(uint8_t ins[], reg_t* cons[], int offset);
+            uint8_t ReadA(uint8_t ins[]);
+            uint8_t ReadB(uint8_t ins[]);
+            uint8_t ReadC(uint8_t ins[]);
+            uint32_t ReadBx(uint8_t ins[]);
+            uint32_t ReadAbx(uint8_t ins[]);
+            void Call(uint8_t a, uint8_t b, uint8_t c);
     };
 }
