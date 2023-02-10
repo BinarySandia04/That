2421e28faceb0bb35338c9b175943a7979869f77
Aran Roig <aranseraroig@gmail.com>
2023-02-10 15:18:40 +0100

    Para funciones internas

---
+++ src/internal.cpp
@@ -0,0 +1,7 @@
+#include "internal.h"
+
+#include <iostream>
+
+void That::Internal::print(int n){
+    std::cout << n << std::endl;
+}
---
+++ src/internal.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+namespace That {
+    namespace Internal {
+        void print(int t);
+    }
+}
